a468a57f57dcce483f4eaf28415f6e5694e255fd
Lahari_naik <182788277+Lahari-Naik@users.noreply.github.com>
2025-04-06 15:27:37 +0530

    Update grid.js

--- Grid-project/grid.js
+++ Grid-project/grid.js
@@ -42,14 +42,8 @@ shuffleCards();
 function toggleLike(button) {
 let countSpan = button.nextElementSibling;
 let count = parseInt(countSpan.textContent);
-
-if (button.classList.contains("liked")) {
-button.classList.remove("liked");
-count--;
-} else {
 button.classList.add("liked");
 count++;
-}
 
 countSpan.textContent = count;
 }
